6e46b1de783b417e2e4608210cc04907f210ee70
yosipy <todacchi.desu@gmail.com>
2024-03-19 19:19:20 +0900

    ssr & csr with react

--- 20240318_hono/vite.config.ts
+++ 20240318_hono/vite.config.ts
@@ -1,14 +1,34 @@
-import build from '@hono/vite-cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-cloudflare-pages"
+import devServer from "@hono/vite-dev-server"
+import { defineConfig } from "vite"
+import path from "path"
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  console.log(mode)
+  console.log(path.resolve(__dirname, "/src/client.tsx"))
+  if (mode === "client") {
+    return {
+      build: {
+        manifest: true,
+        rollupOptions: {
+          input: "/src/client.tsx",
+          output: {
+            entryFileNames: "static/client.js",
+          },
+        },
+      },
+    }
+  } else {
+    return {
+      ssr: {
+        external: ["react", "react-dom"],
+      },
+      plugins: [
+        build(),
+        devServer({
+          entry: "src/index.tsx",
+        }),
+      ],
+    }
+  }
 })
