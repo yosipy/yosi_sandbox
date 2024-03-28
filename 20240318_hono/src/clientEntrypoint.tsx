6e6fdff5ad16f3f626e9de8fde49cf653e7214c5
yosipy <todacchi.desu@gmail.com>
2024-03-29 01:20:02 +0900

    log

--- 20240318_hono/src/clientEntrypoint.tsx
+++ 20240318_hono/src/clientEntrypoint.tsx
@@ -1,6 +1,18 @@
 import { createRoot } from "react-dom/client"
 import { HomuraProvider } from "./homura/provider/HomuraProvider"
 
+const getHeadElements = () => {
+  const head = document.head
+  const titleElement = head.querySelector("title")
+  const linkElements = head.querySelectorAll("link")
+  const metaElements = head.querySelectorAll("meta")
+
+  console.log("Titleタグ:", titleElement?.outerHTML)
+  linkElements.forEach((element) => console.log("Linkタグ:", element.outerHTML))
+  metaElements.forEach((element) => console.log("Metaタグ:", element.outerHTML))
+}
+getHeadElements()
+
 const domNode = document.getElementById("root")!
 const root = createRoot(domNode)
 root.render(<HomuraProvider />)
